8cacd9240be1d7d623bb596c7e543808284f82d8
Sam <sam_ring@hotmail.com>
2020-07-28 16:27:01 +0100

    Updates

---
+++ precache-manifest.e8cde19854fa5da12348501f261626ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6ec0c371a59b1ea59b1392092068458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e92e4774225842c5682f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aa73d2de14fb9a72f31",
+    "url": "/static/js/2.140dc083.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.140dc083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92e4774225842c5682f",
+    "url": "/static/js/main.d2776374.chunk.js"
+  },
+  {
+    "revision": "65459e5e73afc8c585ff",
+    "url": "/static/js/runtime-main.a680a5ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
